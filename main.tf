6322718511950f5c83f6961c22fee905a0c5cd40
ASKamenev <kamenev.ndv@gmail.com>
2022-07-06 18:57:24 +0000

    created tf blueprint

---
+++ main.tf
@@ -0,0 +1,121 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.43.0"
+    }
+     selectel = {
+      source  = "selectel/selectel"
+      version = "~> 3.6.2"
+    }
+     ansible = {
+      source = "habakke/ansible"
+      version = "~> 1.0"
+    }
+  }
+}
+
+# Access data for Openstack
+provider "openstack" {
+  auth_url    = "https://api.selvpc.ru/identity/v3"
+  domain_name = "222924"
+  tenant_id = "6d026ff572a344f5ba7586a33997feef"
+  user_name   = var.username
+  password    = var.password
+  region      = var.region
+}
+
+# Creating key-pair
+resource "openstack_compute_keypair_v2" "key_rabbitmq" {
+  name       = "key_tf"
+  region     = var.region
+  public_key = var.public_key
+}
+
+################ Network Operations ####################
+
+# Filling external network variable
+data "openstack_networking_network_v2" "external_net" {
+  name = "external-network"
+}
+
+# Creating a router
+resource "openstack_networking_router_v2" "router_rabbitmq" {
+  name                = "router_rabbitmq"
+  external_network_id = data.openstack_networking_network_v2.external_net.id
+}
+
+# Creating a network
+resource "openstack_networking_network_v2" "network_rabbitmq" {
+  name = "network_rabbitmq"
+
+# Creating a subnet
+resource "openstack_networking_subnet_v2" "subnet_rabbitmq" {
+  network_id = openstack_networking_network_v2.network_rabbitmq.id
+  name       = "subnet_rabbitmq"
+  cidr       = var.subnet_cidr
+}
+
+# Attaching subnet to the router
+resource "openstack_networking_router_interface_v2" "router_interface_rabbitmq" {
+  router_id = openstack_networking_router_v2.router_rabbitmq.id
+  subnet_id = openstack_networking_subnet_v2.subnet_rabbitmq.id
+}
+
+# Floating address creation
+resource "openstack_networking_floatingip_v2" "fip_rabbitmq" {
+  pool = "external-network"
+}
+
+################ Volume Creation ####################
+
+resource "openstack_blockstorage_volume_v3" "volume_rabbitmq-0" {
+  name                 = "volume_rabbitmq-0"
+  size                 = "20"
+  image_id             = "8f3c4108-de09-4333-87ab-c53523d93557" 
+  volume_type          = var.volume_type  <--- need to check what is this? 
+  availability_zone    = var.az_zone      <--- need to check what is this?
+  enable_online_resize = true
+  lifecycle {
+    ignore_changes = [image_id]
+  }
+}
+
+resource "openstack_blockstorage_volume_v3" "volume_rabbitmq-1" {
+  name                 = "volume_rabbitmq-1"
+  size                 = "20"
+  image_id             = "8f3c4108-de09-4333-87ab-c53523d93557"
+  volume_type          = var.volume_type  <--- need to check what is this?
+  availability_zone    = var.az_zone      <--- need to check what is this?
+  enable_online_resize = true
+  lifecycle {
+    ignore_changes = [image_id]
+  }
+}
+
+resource "openstack_blockstorage_volume_v3" "volume_rabbitmq-2" {
+  name                 = "volume_rabbitmq-2"
+  size                 = "20"
+  image_id             = "8f3c4108-de09-4333-87ab-c53523d93557"
+  volume_type          = var.volume_type  <--- need to check what is this?
+  availability_zone    = var.az_zone      <--- need to check what is this?
+  enable_online_resize = true
+  lifecycle {
+    ignore_changes = [image_id]
+  }
+}
+
+################ Stack Creation ####################
+
+# Deploying first stack
+resource "openstack_compute_instance_v2" "rabbitmq-0" {
+  name            = "rabbitmq-0"
+  flavor_id       = "1011"
+  key_pair        = "key_rabbitmq"
+  security_groups = ["default"]
+
+  network {
+    name = "my_network"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "username" {
+  description = "The username for the DB master user"
+  type        = string
+}
+
+variable "password" {
+  description = "The password for the DB master user"
+  type        = string
+}
+
+variable "region" {
+  default = "ru-1"
+}
+
+# SSh-public-key
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDdslEjLc49sg1aNOCO1jK+gx17EywPGU8dl0wO3yQhSyrN5Ca7fPCwos2Pwhk5yQ3L2McDkBJf1vRPKmd1QzDGfUP67ZXeeTSNjpG0ymH+HREGATJk1IKOOhs949c5ISdKTPTr2XY0Yd9AyeY6gMCEz2eIRKjfG1GPnXGEKc4TQp6ZlRrNlJTw8eVR5EnSqsAT+H8MtqhJoSToJszJq65HfP+eSg5uC72o8dqlfhmDg4UqneqghxgDhpxxqbpPLpxDpektMjC810AXfYnHwy5AA3gmBG8yfzMvoXI3SZboINZa+Argyr2IS3xdg+3VRUws40R4HQfHF6ZziJi1TZy4mFLmesoDPpJmjcoiHse112+ZR4XdVhKbBhfi6/00nWQUJaD9SYYvrTdNgMyskF1zRU/PbqtzhnY5jT4xdaK6vb5mAS2NfKdLgCg1WCaULG9uRz+IUu2VGihyOS1yUpNZITX/9T649yzf2QXZI7JT6oJsNu3TX5IpzaFbzPq9Jns= andrew@IBM-Laptop"
+
+}
+# Зона доступности
+variable "az_zone" {
+  default = "ru-1a"
+}
+# Тип сетевого диска, из которого создается сервер
+variable "volume_type" {
+  default = "fast.ru-1a"
+}
+# CIDR for subnet
+variable "subnet_cidr" {
+  default = "192.168.0.0/29"
+}
