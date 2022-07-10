5daa7e7d265af21c068462d225c053fca9758cb3
ASKamenev <kamenev.ndv@gmail.com>
2022-07-10 18:43:22 +0000

    some improvements

--- receive.py
+++ receive.py
@@ -2,8 +2,8 @@
 import pika, sys, os
 
 def main():
-    credentials = pika.PlainCredentials('rabbit','768a852ed69ce916fa7faa278c962de3e4275e5f')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('95.213.229.113', '5672', '/', credentials))
+    credentials = pika.PlainCredentials('rabbit','<pass>')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('<ip_addr>', '5672', '/', credentials))
     channel = connection.channel()
 
     channel.queue_declare(queue='hello')
--- send.py
+++ send.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import pika
+count = 0
+while count <= 10:
+  credentials = pika.PlainCredentials('rabbit', '768a852ed69ce916fa7faa278c962de3e4275e5f')
+  connection = pika.BlockingConnection(pika.ConnectionParameters('45.136.180.132', '5672', '/', credentials))
+  channel = connection.channel()
 
-credentials = pika.PlainCredentials('rabbit', '768a852ed69ce916fa7faa278c962de3e4275e5f')
-connection = pika.BlockingConnection(pika.ConnectionParameters('95.213.229.113', '5672', '/', credentials))
-channel = connection.channel()
+  channel.queue_declare(queue='hello')
 
-channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+  channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+  print(" [x] Sent 'Hello World!'")
+  count +=1
 connection.close()
